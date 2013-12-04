de5f11b97d1f8936861cf8eb43572277a5bba09f
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2013-12-04 17:10:39 +0100

    create conf_dir

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,11 @@
 # Recipe:: default
 #
 
+directory node['datadog']['conf_dir'] do
+  recursive true
+  action :create
+end
+
 include_recipe "datadog::dd-agent"
 include_recipe "datadog::dd-handler"
 include_recipe "datadog::dogstatsd-ruby"
